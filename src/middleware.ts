05bb88db81a7b8ce42456aca63da3acf4de063a9
Moonjuice <moon.kang@doverunner.com>
2025-05-07 12:10:29 +0900

    Setup middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,36 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from "next/server";
 import { routing } from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+import getSession from './lib/session';
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
+const intlMiddleware = createMiddleware(routing);
+
+export default async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
+  return intlMiddleware(request);
+}
+
 export const config = {
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
